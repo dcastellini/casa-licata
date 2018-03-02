00482707c6ec068f0120186778cd512947e80c8d
dcastellini <dcastellini89@gmail.com>
2018-03-02 17:00:20 -0300

    Comienzo web dinamica

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -21,12 +21,14 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Bienvenido <?php echo Yii::app()->session['usuario']; ?></a></li>
-                <li><a href="#">Configuración</a></li>
+            <li><a href="#">Bienvenido <?php echo Yii::app()->session['usuario']; ?></a></li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    Configuración<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="Logout">Desconectarse</a></li>
+                </ul>
+            </li>
             </ul>
-            <form class="navbar-form navbar-right">
-                <input type="text" class="form-control" placeholder="Search...">
-            </form>
         </div>
     </div>
 </nav>
@@ -35,27 +37,13 @@
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Reports</a></li>
-                <li><a href="#">Analytics</a></li>
-                <li><a href="#">Export</a></li>
-            </ul>
-            <ul class="nav nav-sidebar">
-                <li><a href="">Nav item</a></li>
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-                <li><a href="">More navigation</a></li>
-            </ul>
-            <ul class="nav nav-sidebar">
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-            </ul>
+                <li onclick="changeClass(this.value)" id="1" value="1"><a href="#">Materiales</a></li>
+                <li onclick="changeClass(this.value)" id="2" value="2"><a href="#">Clientes</a></li>
+                <li onclick="changeClass(this.value)" id="3" value="3"><a href="#">Proveedores</a></li>
+                <li onclick="changeClass(this.value)" id="4" value="4"><a href="#">Facturación</a></li>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <h1 class="page-header">¡Hola <?php echo Yii::app()->session['usuario']; ?>! ¿Qué deseas hacer? </h1>
-
             <div class="row placeholders">
                 <div class="col-xs-6 col-sm-3 placeholder">
                     <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
@@ -78,135 +66,56 @@
                     <span class="text-muted">Something else</span>
                 </div>
             </div>
-
-            <h2 class="sub-header">Section title</h2>
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>Lorem</td>
-                        <td>ipsum</td>
-                        <td>dolor</td>
-                        <td>sit</td>
-                    </tr>
-                    <tr>
-                        <td>1,002</td>
-                        <td>amet</td>
-                        <td>consectetur</td>
-                        <td>adipiscing</td>
-                        <td>elit</td>
-                    </tr>
-                    <tr>
-                        <td>1,003</td>
-                        <td>Integer</td>
-                        <td>nec</td>
-                        <td>odio</td>
-                        <td>Praesent</td>
-                    </tr>
-                    <tr>
-                        <td>1,003</td>
-                        <td>libero</td>
-                        <td>Sed</td>
-                        <td>cursus</td>
-                        <td>ante</td>
-                    </tr>
-                    <tr>
-                        <td>1,004</td>
-                        <td>dapibus</td>
-                        <td>diam</td>
-                        <td>Sed</td>
-                        <td>nisi</td>
-                    </tr>
-                    <tr>
-                        <td>1,005</td>
-                        <td>Nulla</td>
-                        <td>quis</td>
-                        <td>sem</td>
-                        <td>at</td>
-                    </tr>
-                    <tr>
-                        <td>1,006</td>
-                        <td>nibh</td>
-                        <td>elementum</td>
-                        <td>imperdiet</td>
-                        <td>Duis</td>
-                    </tr>
-                    <tr>
-                        <td>1,007</td>
-                        <td>sagittis</td>
-                        <td>ipsum</td>
-                        <td>Praesent</td>
-                        <td>mauris</td>
-                    </tr>
-                    <tr>
-                        <td>1,008</td>
-                        <td>Fusce</td>
-                        <td>nec</td>
-                        <td>tellus</td>
-                        <td>sed</td>
-                    </tr>
-                    <tr>
-                        <td>1,009</td>
-                        <td>augue</td>
-                        <td>semper</td>
-                        <td>porta</td>
-                        <td>Mauris</td>
-                    </tr>
-                    <tr>
-                        <td>1,010</td>
-                        <td>massa</td>
-                        <td>Vestibulum</td>
-                        <td>lacinia</td>
-                        <td>arcu</td>
-                    </tr>
-                    <tr>
-                        <td>1,011</td>
-                        <td>eget</td>
-                        <td>nulla</td>
-                        <td>Class</td>
-                        <td>aptent</td>
-                    </tr>
-                    <tr>
-                        <td>1,012</td>
-                        <td>taciti</td>
-                        <td>sociosqu</td>
-                        <td>ad</td>
-                        <td>litora</td>
-                    </tr>
-                    <tr>
-                        <td>1,013</td>
-                        <td>torquent</td>
-                        <td>per</td>
-                        <td>conubia</td>
-                        <td>nostra</td>
-                    </tr>
-                    <tr>
-                        <td>1,014</td>
-                        <td>per</td>
-                        <td>inceptos</td>
-                        <td>himenaeos</td>
-                        <td>Curabitur</td>
-                    </tr>
-                    <tr>
-                        <td>1,015</td>
-                        <td>sodales</td>
-                        <td>ligula</td>
-                        <td>in</td>
-                        <td>libero</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function changeClass(value){
+        debugger;
+        var array = [];
+        for(var i=1;i<=20;i++){
+            if(i>= 1 && i<=5){
+                $('#'+i+'').removeClass('active');
+            }
+            else{
+                $('#'+i+'').remove();
+            }
+        }
+        $('#'+value+'').addClass("active");
+
+        if(value == 1){
+            $("<li onclick='crearMaterial()' id='6' value='6'><a href='#'>Crear Material</a></li>").insertAfter('#1');
+            $("<li id='7' value='7'><a href='#'>Modificar Material</a></li>").insertAfter('#1');
+            $("<li id='8' value='8'><a href='#'>Modificar Stock</a></li>").insertAfter('#1');
+            $("<li id='9' value='9'><a href='#'>Consultar Stock</a></li>").insertAfter('#1');
+            $("<li id='10' value='10'><a href='#'>Eliminar Material</a></li>").insertAfter('#1');
+        }
+        if(value == 2){
+            $("<li id='11' value='11'><a href='#'>Crear Cliente</a></li>").insertAfter('#2');
+            $("<li id='12' value='12'><a href='#'>Consultar Cliente</a></li>").insertAfter('#2');
+            $("<li id='13' value='13'><a href='#'>Modificar Cliente</a></li>").insertAfter('#2');
+            $("<li id='14' value='14'><a href='#'>Eliminar Cliente</a></li>").insertAfter('#2');
+        }
+        if(value == 3){
+            $("<li id='15' value='15'><a href='#'>Crear Proveedor</a></li>").insertAfter('#3');
+            $("<li id='16' value='16'><a href='#'>Consultar Proveedor</a></li>").insertAfter('#3');
+            $("<li id='17' value='17'><a href='#'>Modificar Proveedor</a></li>").insertAfter('#3');
+            $("<li id='18' value='18'><a href='#'>Eliminar Proveedor</a></li>").insertAfter('#3');
+        }
+        if(value == 4){
+            $("<li id='19' value='19'><a href='#'>Consultar Facturar</a></li>").insertAfter('#4');
+            $("<li id='20' value='20'><a href='#'>Anular Factura</a></li>").insertAfter('#4');
+        }
+
+    }
+
+
+</script>
+
+<script type="text/javascript">
+    function crearMaterial(){
+        
+    }
+
+</script>
